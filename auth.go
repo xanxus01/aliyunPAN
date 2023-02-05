48e7c8a00870894d57d5de80f5af66e1c440e8c7
chenhuachao <chenhuachao@4399inc.com>
2023-02-05 17:25:00 +0800

    QrCode Callback

--- auth.go
+++ auth.go
@@ -62,16 +62,19 @@ type QrCodeStateRes struct {
 	HasError bool
 }
 
-func Login()  {
+func Login() {
 
 }
 
-
-func QrLogin() (d *Driver, err error) {
+func QrLogin(qrCodeCallback ...func(codeContent string)) (d *Driver, err error) {
 	generateRes, err := Generate()
 	if err != nil {
 		return
 	}
+	for _, callback := range qrCodeCallback {
+		callback(generateRes.Content.Data.CodeContent)
+	}
+
 	ck := generateRes.Content.Data.CK
 	t := generateRes.Content.Data.T
 	for true {
@@ -91,9 +94,9 @@ func QrLogin() (d *Driver, err error) {
 			break
 		}
 		if qrCodeStatus == "CONFIRMED" {
-			token,errParseBiz := ParseBiz(qrCodeRes.Content.Data.BizExt)
+			token, errParseBiz := ParseBiz(qrCodeRes.Content.Data.BizExt)
 			if errParseBiz != nil {
-				return nil,errParseBiz
+				return nil, errParseBiz
 			}
 			d, err = NewDriver(Option{Token: token})
 			log(d, err, qrCodeRes)
@@ -119,26 +122,11 @@ func Generate() (res QrCodeRes, err error) {
 	if err != nil {
 		return
 	}
-	//bodyStr, _ := ioutil.ReadAll(rsp.Body)
-	//log(rsp.StatusCode)
-	//log(string(bodyStr))
-	//log(res)
-	filePath := "E:\\Documents\\a.png"
-	err = qrcode.WriteFile(res.Content.Data.CodeContent, qrcode.Low, 500, filePath)
-	openQrCode(filePath)
+
 	return
 }
 
-func openQrCode(path string) {
-	cmd := exec.Command("cmd", "/C", path)
-	err := cmd.Start()
-	if err != nil {
-		panic(err.Error())
-	}
-}
-
 func QrCodeQuery(ck string, t int) (res QrCodeStateRes, err error) {
-	//urlStr := "https://passport.aliyundrive.com/newlogin/qrcode/query.do?appName=aliyun_drive&fromSite=52&_bx-v=2.2.3"
 	urlStr := passportHost + "/newlogin/qrcode/query.do?appName=aliyun_drive&fromSite=52&_bx-v=2.2.3"
 	params := url.Values{}
 	params.Set("ck", ck)
@@ -160,54 +148,54 @@ func QrCodeQuery(ck string, t int) (res QrCodeStateRes, err error) {
 	return
 }
 
-func TokenLogin(token string) (string,error) {
+func TokenLogin(token string) (string, error) {
 	urlStr := authPortHost + "/v2/oauth/token_login"
 	params := map[string]string{"token": token}
 	data, err := json.Marshal(params)
 	if err != nil {
-		return "",err
+		return "", err
 	}
 	log(string(data))
 	req, err := http.NewRequest("POST", urlStr, bytes.NewReader(data))
 	if err != nil {
-		return "",err
+		return "", err
 	}
 
-	cookies,err := Authorize()
+	cookies, err := Authorize()
 	if err != nil {
-		return "",err
+		return "", err
 	}
 	log(cookies)
-	for _,v :=range cookies{
+	for _, v := range cookies {
 		req.AddCookie(v)
 	}
-	req.Header.Set("content-type","application/json;charset=UTF-8")
+	req.Header.Set("content-type", "application/json;charset=UTF-8")
 	//req.Header.Set("referer","https://auth.aliyundrive.com/v2/oauth/authorize?client_id=25dzX3vbYqktVxyX&redirect_uri=https%3A%2F%2Fwww.aliyundrive.com%2Fsign%2Fcallback&response_type=code&login_type=custom&state=%7B%22origin%22%3A%22https%3A%2F%2Fwww.aliyundrive.com%22%7D")
 	res := make(map[string]string)
 	rsp, err := doAndParse(req, &res)
 
 	if err != nil {
-		return "",err
+		return "", err
 	}
-	log(res,req.Header.Get("Cookie"), rsp.StatusCode, rsp.StatusCode)
+	log(res, req.Header.Get("Cookie"), rsp.StatusCode, rsp.StatusCode)
 	if res["goto"] == "" {
-		err = newError(fmt.Sprintf("goto is empty, url %s ,statusCode %d, status %s, token %s",urlStr, rsp.StatusCode, rsp.Status, token))
-		return "",err
+		err = newError(fmt.Sprintf("goto is empty, url %s ,statusCode %d, status %s, token %s", urlStr, rsp.StatusCode, rsp.Status, token))
+		return "", err
 	}
 	gotoUrl := res["goto"]
-	return gotoUrl,err
+	return gotoUrl, err
 }
 
-func BizLogin(biz string)(*Driver,error){
-	token,errParseBiz := ParseBiz(biz)
+func BizLogin(biz string) (*Driver, error) {
+	token, errParseBiz := ParseBiz(biz)
 	if errParseBiz != nil {
-		return nil,errParseBiz
+		return nil, errParseBiz
 	}
 	d, err := NewDriver(Option{Token: token})
 	return d, err
 }
 
-func (d *Driver) getToken(code string) (error) {
+func (d *Driver) getToken(code string) error {
 	urlStr := apiHost + "/token/get"
 	params := map[string]string{
 		"code":      code,
@@ -217,7 +205,7 @@ func (d *Driver) getToken(code string) (error) {
 	if err != nil {
 		return err
 	}
-	req.Header.Set("content-type","application/json;charset=UTF-8")
+	req.Header.Set("content-type", "application/json;charset=UTF-8")
 
 	res := TokenObj{}
 	r, err := doAndParse(req, &res)
@@ -225,12 +213,12 @@ func (d *Driver) getToken(code string) (error) {
 		return err
 	}
 	log("doAndParse", string(r.Data))
-	fmt.Printf("%+v",res)
-	data,errJson := json.Marshal(res)
+	fmt.Printf("%+v", res)
+	data, errJson := json.Marshal(res)
 	log("json.Marshal")
 	if errJson != nil {
 		log(errJson.Error())
-	}else {
+	} else {
 		log("json.Marshal success")
 		Write(string(data), TokenFile())
 	}
@@ -240,7 +228,7 @@ func (d *Driver) getToken(code string) (error) {
 
 func NewDriverWithTokenObj(obj TokenObj) (*Driver, error) {
 	d := &Driver{
-		token: obj.AccessToken,
+		token:    obj.AccessToken,
 		tokenObj: obj,
 	}
 	// Expire
@@ -254,25 +242,25 @@ func NewDriverWithTokenObj(obj TokenObj) (*Driver, error) {
 	if err != nil {
 		return nil, err
 	}
-	return d,nil
+	return d, nil
 }
 
 func Authorize() ([]*http.Cookie, error) {
 	urlStr := "https://auth.aliyundrive.com/v2/oauth/authorize?client_id=25dzX3vbYqktVxyX&redirect_uri=https%3A%2F%2Fwww.aliyundrive.com%2Fsign%2Fcallback&response_type=code&login_type=custom&state=%7B%22origin%22%3A%22https%3A%2F%2Fwww.aliyundrive.com%22%7D"
 	req, err := http.NewRequest("GET", urlStr, nil)
 	if err != nil {
-		return  nil,err
+		return nil, err
 	}
 	response, err := http.DefaultClient.Do(req)
 	if err != nil {
-		return nil,err
+		return nil, err
 	}
 
-	return response.Cookies(),err
+	return response.Cookies(), err
 }
 
-func Write(s string, file string)  {
-	fmt.Println("Write",s)
+func Write(s string, file string) {
+	fmt.Println("Write", s)
 	f, err := os.OpenFile(file, os.O_CREATE, 0666)
 	if err != nil {
 		panic(err.Error())
@@ -283,17 +271,17 @@ func Write(s string, file string)  {
 	}
 }
 
-func ParseBiz(s string) (string,error) {
+func ParseBiz(s string) (string, error) {
 	m := make(map[string]map[string]interface{})
 	decoded, err := base64.StdEncoding.DecodeString(s)
 	if err == nil {
 		err = json.Unmarshal(decoded, &m)
 	}
-	token,ok := (m["pds_login_result"]["accessToken"]).(string)
+	token, ok := (m["pds_login_result"]["accessToken"]).(string)
 	if !ok {
 		err = newError("token error:" + s)
 	}
-	return token,err
+	return token, err
 }
 
 func TmpDir() string {
@@ -308,14 +296,14 @@ func TmpDir() string {
 	return tmpFile
 }
 
-func init()  {
+func init() {
 	tmpDir := TmpDir()
-	f,err := os.Stat(tmpDir)
+	f, err := os.Stat(tmpDir)
 
-	if err !=nil {
+	if err != nil {
 		panic(err.Error())
 	}
-	if !f.IsDir(){
+	if !f.IsDir() {
 		err = os.Mkdir(tmpDir, 0766)
 		if err != nil {
 			panic(err.Error())
@@ -331,4 +319,21 @@ func TokenFile() string {
 
 func BizFile() string {
 	return bizFile
+}
+
+func OpenCodeContent(codeContent string) {
+	filePath := "E:\\Documents\\a.png"
+	err := qrcode.WriteFile(codeContent, qrcode.Low, 500, filePath)
+	if err != nil {
+		fmt.Println(err.Error())
+	}
+	openQrCode(filePath)
+}
+
+func openQrCode(path string) {
+	cmd := exec.Command("cmd", "/C", path)
+	err := cmd.Start()
+	if err != nil {
+		panic(err.Error())
+	}
 }
